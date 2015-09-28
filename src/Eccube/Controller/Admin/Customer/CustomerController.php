a8ad17255883ea34386eeb28e98c47203f4de5e2
vinh.hieu <vinh.hieu>
2015-09-28 11:31:25 +0700

    #ECCUBE3

---
+++ src/Eccube/Controller/Admin/Customer/CustomerController.php
@@ -0,0 +1,162 @@
+<?php
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) 2000-2015 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+namespace Eccube\Controller\Admin\Customer;
+
+use Eccube\Application;
+use Eccube\Entity\Master\CsvType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class CustomerController
+{
+    public function index(Application $app, Request $request)
+    {
+        $pagination = null;
+        $searchForm = $app['form.factory']
+            ->createBuilder('admin_search_customer')
+            ->getForm();
+
+        $searchForm->handleRequest($request);
+        $searchData = array();
+        if ($searchForm->isValid()) {
+            $searchData = $searchForm->getData();
+        }
+
+        if ('POST' === $request->getMethod()) {
+            $qb = $app['eccube.repository.customer']
+                ->getQueryBuilderBySearchData($searchData);
+
+            $pagination = $app['paginator']()->paginate(
+                $qb,
+                empty($searchData['pageno']) ? 1 : $searchData['pageno'],
+                empty($searchData['pagemax']) ? 10 : $searchData['pagemax']->getId()
+            );
+        }
+
+        return $app->render('Customer/index.twig', array(
+            'searchForm' => $searchForm->createView(),
+            'pagination' => $pagination,
+        ));
+    }
+
+    public function resend(Application $app, Request $request, $id)
+    {
+        $Customer = $app['orm.em']
+            ->getRepository('Eccube\Entity\Customer')
+            ->find($id);
+
+        if (is_null($Customer)) {
+            throw new NotFoundHttpException();
+        }
+
+        $activateUrl = $app->url('entry_activate', array('secret_key' => $Customer->getSecretKey()));
+
+        // メール送信
+        $app['eccube.service.mail']->sendAdminCustomerConfirmMail($Customer, $activateUrl);
+
+        $app->addSuccess('admin.customer.resend.complete', 'admin');
+
+        return $app->redirect($app->url('admin_customer'));
+    }
+
+    public function delete(Application $app, Request $request, $id)
+    {
+        $Customer = $app['orm.em']
+            ->getRepository('Eccube\Entity\Customer')
+            ->find($id);
+
+        if (is_null($Customer)) {
+            throw new NotFoundHttpException();
+        }
+
+        $Customer->setDelFlg(1);
+        $app['orm.em']->persist($Customer);
+        $app['orm.em']->flush();
+        $app->addSuccess('admin.customer.delete.complete', 'admin');
+
+        return $app->redirect($app->url('admin_customer'));
+    }
+
+    /**
+     * 会員CSVの出力.
+     * @param Application $app
+     * @param Request $request
+     * @return StreamedResponse
+     */
+    public function export(Application $app, Request $request)
+    {
+        // タイムアウトを無効にする.
+        set_time_limit(0);
+
+        // sql loggerを無効にする.
+        $em = $app['orm.em'];
+        $em->getConfiguration()->setSQLLogger(null);
+
+        $response = new StreamedResponse();
+        $response->setCallback(function () use ($app, $request) {
+
+            // CSV種別を元に初期化.
+            $app['eccube.service.csv.export']->initCsvType(CsvType::CSV_TYPE_CUSTOMER);
+
+            // ヘッダ行の出力.
+            $app['eccube.service.csv.export']->exportHeader();
+
+            // 会員データ検索用のクエリビルダを取得.
+            $qb = $app['eccube.service.csv.export']
+                ->getCustomerQueryBuilder($request);
+
+            // データ行の出力.
+            $app['eccube.service.csv.export']->setExportQueryBuilder($qb);
+            $app['eccube.service.csv.export']->exportData(function ($entity, $csvService) {
+
+                $Csvs = $csvService->getCsvs();
+
+                /** @var $Customer \Eccube\Entity\Customer */
+                $Customer = $entity;
+
+                $row = array();
+
+                // CSV出力項目と合致するデータを取得.
+                foreach ($Csvs as $Csv) {
+                    // 会員データを検索.
+                    $row[] = $csvService->getData($Csv, $Customer);
+                }
+
+                //$row[] = number_format(memory_get_usage(true));
+                // 出力.
+                $csvService->fputcsv($row);
+            });
+        });
+
+        $now = new \DateTime();
+        $filename = 'customer_' . $now->format('YmdHis') . '.csv';
+        $response->headers->set('Content-Type', 'application/octet-stream');
+        $response->headers->set('Content-Disposition', 'attachment; filename=' . $filename);
+        $response->send();
+
+        return $response;
+    }
+}
---
+++ src/Eccube/Util/Str.php
@@ -0,0 +1,248 @@
+<?php
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) 2000-2015 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+namespace Eccube\Util;
+
+class Str
+{
+
+    /**
+     * The MIT License (MIT)
+     *
+     * Copyright (c) <Taylor Otwell>
+     *
+     * Permission is hereby granted, free of charge, to any person obtaining a copy
+     * of this software and associated documentation files (the "Software"), to deal
+     * in the Software without restriction, including without limitation the rights
+     * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+     * copies of the Software, and to permit persons to whom the Software is
+     * furnished to do so, subject to the following conditions:
+     *
+     * The above copyright notice and this permission notice shall be included in
+     * all copies or substantial portions of the Software.
+     *
+     * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+     * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+     * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+     * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+     * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+     * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+     * THE SOFTWARE
+     *
+     * Generate a more truly "random" alpha-numeric string.
+     *
+     * @param  int $length
+     * @return string
+     *
+     * @throws \RuntimeException
+     */
+    public static function random($length = 16)
+    {
+        if (function_exists('openssl_random_pseudo_bytes')) {
+            $bytes = openssl_random_pseudo_bytes($length * 2);
+
+            if ($bytes === false) {
+                throw new \RuntimeException('Unable to generate random string.');
+            }
+
+            return substr(str_replace(array('/', '+', '='), '', base64_encode($bytes)), 0, $length);
+        }
+
+        return static::quickRandom($length);
+    }
+
+    /**
+     * The MIT License (MIT)
+     *
+     * Copyright (c) <Taylor Otwell>
+     *
+     * Permission is hereby granted, free of charge, to any person obtaining a copy
+     * of this software and associated documentation files (the "Software"), to deal
+     * in the Software without restriction, including without limitation the rights
+     * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+     * copies of the Software, and to permit persons to whom the Software is
+     * furnished to do so, subject to the following conditions:
+     *
+     * The above copyright notice and this permission notice shall be included in
+     * all copies or substantial portions of the Software.
+     *
+     * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+     * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+     * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+     * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+     * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+     * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+     * THE SOFTWARE
+     *
+     * Generate a "random" alpha-numeric string.
+     *
+     * Should not be considered sufficient for cryptography, etc.
+     *
+     * @param  int $length
+     * @return string
+     */
+    public static function quickRandom($length = 16)
+    {
+        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        return substr(str_shuffle(str_repeat($pool, $length)), 0, $length);
+    }
+
+
+    /**
+     * 改行コードの変換
+     *
+     * @param $value
+     * @param string $lf
+     * @return string
+     */
+    public static function convertLineFeed($value, $lf = "\n")
+    {
+        if (empty($value)) {
+            return '';
+        }
+        return strtr($value, array_fill_keys(array("\r\n", "\r", "\n"), $lf));
+    }
+
+    /**
+     * 文字コードの判定
+     *
+     * @param $value
+     * @return string
+     */
+    public static function characterEncoding($value, $encoding = array('UTF-8', 'SJIS', 'EUC-JP', 'ASCII', 'JIS', 'sjis-win'))
+    {
+        foreach ($encoding as $encode) {
+            if (mb_convert_encoding($value, $encode, $encode) == $value) {
+                return $encode;
+            }
+        }
+
+        return null;
+
+    }
+
+    /**
+     * 指定した文字列以上ある場合、「...」を付加する
+     *
+     * @param string $value
+     * @param int $length
+     * @param string $end
+     * @return string
+     */
+    public static function ellipsis($value, $length = 100, $end = '...')
+    {
+        if (mb_strlen($value) <= $length) {
+            return $value;
+        }
+
+        return rtrim(mb_substr($value, 0, $length, 'UTF-8')) . $end;
+    }
+
+
+    /**
+     *
+     * @param $date
+     * @return string
+     */
+    public static function timeAgo($date)
+    {
+        if (empty($date)) {
+            return '';
+        }
+
+        $now = new \DateTime();
+        if (!($date instanceof \DateTime)) {
+            $date = new \DateTime($date);
+        }
+        $diff = $date->diff($now, true);
+        if ($diff->y > 0) {
+            // return $date->format("Y/m/d H:i");
+            return $date->format("Y/m/d");
+        }
+        if ($diff->m == 1 || $diff->days > 0) {
+            if ($diff->days <= 31) {
+                return $diff->days . '日前';
+            }
+            // return $date->format("Y/m/d H:i");
+            return $date->format("Y/m/d");
+        }
+        if ($diff->h > 0) {
+            return $diff->h . "時間前";
+        }
+        if ($diff->i > 0) {
+            return $diff->i . "分前";
+        }
+        return $diff->s . "秒前";
+    }
+
+    /**
+     * @param $value
+     * @return bool
+     */
+    public static function isBlank($value)
+    {
+        if (!is_string($value)) {
+            return false;
+        }
+
+        if ($value == null || $value == '') {
+            return true;
+        }
+
+        if (trim(mb_convert_kana($value, 's', 'UTF-8')) !== '') {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param $value
+     * @return bool
+     */
+    public static function isNotBlank($value)
+    {
+        return !self::isBlank($value);
+    }
+
+
+    /**
+     * 両端にある全角スペース、半角スペースを取り除く
+     *
+     * @param $value
+     * @return string
+     */
+    public static function trimAll($value)
+    {
+        if ($value == null) {
+            return null;
+        }
+        if ($value == '') {
+            return '';
+        }
+
+        return trim(mb_convert_kana($value, 's', 'UTF-8'));
+    }
+
+}
